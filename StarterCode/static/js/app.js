3602a0a535182d007b94783dc02aeefb88ed903c
Destany Brown <Georgiafbi@gmail.com>
2021-04-06 18:37:55 -0400

    created inital plotly function for selecting different datasets

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,6 +13,18 @@ d3.json("/StarterCode/samples.json").then(function (data) {
         var option=dropDown.append("option").text(name);
         option.attr("value",name);
     });
+    d3.selectAll('#selDataset').on('change',updatePlotly);
+    function updatePlotly(){
+        var dropDownMenu = d3.select('#selDataset');
+        var dataset = dropDownMenu.property("onchange");
+
+        console.log(dataset);
+    }
+    //  {
+    //     conseole.log(this);
+    //     var listItem = d3.select(this);
+    //     console.log(listItem.property("value"));
+    // });
     // Object.entries(data).forEach(element => {
     //     console.log(element);
     //     element.forEach(ele => {
