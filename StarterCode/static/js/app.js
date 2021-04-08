e7f4596c1f1acebec9efa9bbed41f6af22d04764
Destany Brown <Georgiafbi@gmail.com>
2021-04-07 21:57:11 -0400

    added demographic info and bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,24 +2,56 @@
 // var json_path="/StarterCode/samples.json"
 
 d3.json("/StarterCode/samples.json").then(function (data) {
+    //console.log(data);
     var names = data.names;
-    var metadata = data.metadata;
-    var samples = data.samples;
-    console.log(names);
-    console.log(samples);
-    console.log(metadata);
+    //var samples = data.samples;
+    //console.log(names);
     names.forEach(name => {
-        dropDown=d3.select("#selDataset");
-        var option=dropDown.append("option").text(name);
-        option.attr("value",name);
+        dropDown = d3.select("#selDataset");
+        var option = dropDown.append("option").text(name);
+        option.attr("value", name);
     });
-    d3.selectAll('#selDataset').on('change',updatePlotly);
-    function updatePlotly(){
+    var initSubjectID = d3.select("#selDataset>option").node().value
+    demoInfo(initSubjectID);
+    d3.selectAll('#selDataset').on('change', getDataID);
+    function getDataID() {
         var dropDownMenu = d3.select('#selDataset');
-        var dataset = dropDownMenu.property("onchange");
-
-        console.log(dataset);
+        var datasetID = dropDownMenu.node().value;
+        // console.log(datasetID);
+        demoInfo(datasetID);
     }
+    function demoInfo(subjectID) {
+        var metadata = data.metadata;
+        var sample = data.samples;
+        // console.log(subjectID);
+        //console.log(metadata)
+        //console.log(sample)
+        var resultMeta = metadata.filter(mdata => mdata.id === parseInt(subjectID))[0];
+        var resultSample = sample.filter(smple => smple.id === subjectID)[0];
+        //console.log(resultMeta);
+        var panel = d3.select("#sample-metadata");
+        var bar = d3.select("#bar");
+        panel.html("");
+        Object.entries(resultMeta).forEach(([key, value]) => {
+            panel.append("h6").text(`${key.toUpperCase()}: ${value}`).style("color", "red");
+
+        });
+        console.log(resultSample);
+        var values = resultSample.sample_values;
+
+        var trace = {
+            x: values.slice(0, 10),
+            y: resultSample.otu_ids.slice(0, 10),
+            type: 'bar',
+            orientation: 'h'
+
+        };
+        var data1 = [trace];
+        Plotly.newPlot('bar', data1);
+    }
+
+});
+
     //  {
     //     conseole.log(this);
     //     var listItem = d3.select(this);
@@ -31,5 +63,5 @@ d3.json("/StarterCode/samples.json").then(function (data) {
     //         console.log(ele.names);
     //     });
     // });
-});
+
 
