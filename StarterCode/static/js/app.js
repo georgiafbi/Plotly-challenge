b93f94dcdefcc2a4d59c663cfc8c6f4f8c021014
Destany Brown <Georgiafbi@gmail.com>
2021-04-10 02:34:26 -0400

    bubble chart, gauge chart, info panel, bar chart, created

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,6 +2,19 @@
 // var json_path="/StarterCode/samples.json"
 
 d3.json("/StarterCode/samples.json").then(function (data) {
+    buildDropDown(data);
+    var initSubjectID = d3.select("#selDataset>option").node().value
+    visualizations(data, initSubjectID);
+    d3.selectAll('#selDataset').on('change', getDataID);
+    function getDataID() {
+        var dropDownMenu = d3.select('#selDataset');
+        var datasetID = dropDownMenu.node().value;
+        // console.log(datasetID);
+        visualizations(data, datasetID);
+    }
+
+});
+function buildDropDown(data) {
     //console.log(data);
     var names = data.names;
     //var samples = data.samples;
@@ -11,100 +24,130 @@ d3.json("/StarterCode/samples.json").then(function (data) {
         var option = dropDown.append("option").text(name);
         option.attr("value", name);
     });
-    var initSubjectID = d3.select("#selDataset>option").node().value
-    demoInfo(initSubjectID);
-    d3.selectAll('#selDataset').on('change', getDataID);
-    function getDataID() {
-        var dropDownMenu = d3.select('#selDataset');
-        var datasetID = dropDownMenu.node().value;
-        // console.log(datasetID);
-        demoInfo(datasetID);
+}
+function visualizations(data, subjectID) {
+    var metadata = data.metadata;
+    var sample = data.samples;
+    var resultMeta = metadata.filter(mdata => mdata.id === parseInt(subjectID))[0];
+    var resultSample = sample.filter(smple => smple.id === subjectID)[0];
+    buildPanel(resultMeta, metadata);
+    buildHBar(resultSample, sample);
+    buildGauge(resultMeta, metadata);
+    buildBubble(resultSample, sample);
+}
+function buildPanel(resultMeta, metadata) {
+    var panel = d3.select("#sample-metadata");
+    var bar = d3.select("#bar");
+    panel.html("");
+    Object.entries(resultMeta).forEach(([key, value]) => {
+        panel.append("h6").text(`${key.toUpperCase()}: ${value}`).style("color", "Black").style("font-weight","bold");
+
+    });
+
+}
+function buildHBar(resultSample, sample) {
+    console.log(resultSample);
+    var values = resultSample.sample_values;
+    var barTicks = resultSample.otu_ids.slice(0, 10).map((item) => {
+
+        return `OTU ${item}`
+
+    });
+    var traceBar = {
+        x: values.slice(0, 10).reverse(),
+        y: barTicks.reverse(),
+        type: 'bar',
+        orientation: 'h'
+
+    };
+    var dataBar = [traceBar];
+    Plotly.newPlot('bar', dataBar);
+}
+function buildGauge(resultMeta, metadata) {
+    //finding the current frequency for subject ID
+    var wfreq = resultMeta.wfreq;
+    // console.log(wfreq);
+
+    //finding the max frequency
+    var wfreqArray = metadata.filter(wash => wash.wfreq);
+    var maxFreq = d3.max(wfreqArray.map(freq => freq.wfreq));
+
+    //calculating the steps for the gauge
+    var gaugeSteps = [];
+    var colors = coloring(maxFreq);
+    for (var i = 0; i < maxFreq; i++) {
+        //pushing steps and colors to a gaugeSteps list
+        gaugeSteps.push({ range: [i, i + 1], color: colors[i] });
     }
-    function demoInfo(subjectID) {
-        var metadata = data.metadata;
-        var sample = data.samples;
-        // console.log(subjectID);
-        console.log(metadata)
-        console.log(sample)
-        var resultMeta = metadata.filter(mdata => mdata.id === parseInt(subjectID))[0];
-        var resultSample = sample.filter(smple => smple.id === subjectID)[0];
-        //console.log(resultMeta);
-        var panel = d3.select("#sample-metadata");
-        var bar = d3.select("#bar");
-        panel.html("");
-        Object.entries(resultMeta).forEach(([key, value]) => {
-            panel.append("h6").text(`${key.toUpperCase()}: ${value}`).style("color", "red");
-
-        });
-        console.log(resultSample);
-        var values = resultSample.sample_values;
-        var barTicks = resultSample.otu_ids.slice(0, 10).map((item) => {
-
-            return `OTU ${item}`
-
-        });
-        var traceBar = {
-            x: values.slice(0, 10).reverse(),
-            y: barTicks.reverse(),
-            type: 'bar',
-            orientation: 'h'
-
-        };
-        var dataBar = [traceBar];
-        Plotly.newPlot('bar', dataBar);
-        var wfreqArray = metadata.filter(wash => wash.wfreq >= 0);
-        console.log(wfreqArray);
-        var wfreq = resultMeta.wfreq;
-        console.log(wfreq);
-
-        var traceGauge = {
-            type: "indicator",
-            mode: "gauge+number+delta",
-            value: wfreq,
-            title: 'Belly Button Washing Frequency',
-            delta: {
-                reference: 0, increasing: { color: "RebeccaPurple" },
-                gauge: {
-                    axis: { range: [null, Math.max(wfreqArray)], tickwidth: 1, tickcolor: "darkblue" },
-                    bar: { color: "darkblue" },
-                    bgcolor: "white",
-                    borderwidth: 2,
-                    bordercolor: "gray",
-                    steps: [
-                        { range: [0, 1], color: "#00FFFF" },
-                        { range: [1, 2], color: "#7FFFD4" },
-                        { range: [2, 3], color: "#AFE1AF" },
-                        { range: [3, 4], color: "#40E0D0" },
-                        { range: [4, 5], color: "#00FF7F" },
-                        { range: [5, 6], color: "#C4B454" },
-                        { range: [6, 7], color: "#C9CC3F" },
-                        { range: [7, 8], color: "#96DED1" },
-                        { range: [8, 9], color: "#454B1B" },
-                    ],
-                    threshold: {
-                        line: { color: "red", width: 4 },
-                        thickness: 0.75,
-                        value: 490
-                    }
-                }
+    console.log(gaugeSteps);
+    if (wfreq === null) {
+        wfreq = 0;
+    }
+    //defining the gauge trace
+    var traceGauge = {
+        type: "indicator",
+        mode: "gauge+number",
+        value: wfreq,
+        title: "Belly Button Washing Frequency<br>Scrubs per Week",
+        gauge: {
+            axis: { range: [null, maxFreq], tickwidth: 1, tickcolor: "darkblue" },
+            bar: { color: "yellow" },
+            bgcolor: "white",
+            borderwidth: 4,
+            bordercolor: "gray",
+            steps: gaugeSteps,
+            threshold: {
+                line: { color: "red", width: 5 },
+                thickness: 0.75,
+                value: wfreq
             }
         }
-        var dataGauge = [traceGauge];
-        Plotly.newPlot('gauge', dataGauge);
+
+    }
+    var dataGauge = [traceGauge];
+    Plotly.newPlot('gauge', dataGauge);
+
+}
+function buildBubble(resultSample, sample) {
+    var xAxis = resultSample.otu_ids;
+    var yAxis = resultSample.sample_values;
+    console.log(resultSample);
+    var xAxisLength = xAxis.length;
+    var markerColors = coloring(xAxisLength);
+    var opacityNum = randomNum(xAxisLength);
+
+    bubbleTrace = {
+
+        x: xAxis,
+        y: yAxis,
+        mode: 'markers',
+        marker: { color: markerColors, opacity: opacityNum, size: yAxis }
     }
 
-});
+    var bubbleData = [bubbleTrace];
+    var bubbleLayout={
+        xaxis:{title:"OTU ID"}
+    }
+    Plotly.newPlot('bubble',bubbleData,bubbleLayout);
 
-    //  {
-    //     conseole.log(this);
-    //     var listItem = d3.select(this);
-    //     console.log(listItem.property("value"));
-    // });
-    // Object.entries(data).forEach(element => {
-    //     console.log(element);
-    //     element.forEach(ele => {
-    //         console.log(ele.names);
-    //     });
-    // });
+};
 
+function coloring(numColor) {
+    var colorsList = [];
+    for (var i = 0; i < numColor; i++) {
+        //defining a color in hex
+        var color = Math.floor((Math.random() * 1000000) + 1);
+
+        colorsList.push("#" + ("000000" + color.toString(16)).slice(-6));
+    }
+
+    return colorsList;
+}
+function randomNum(num) {
+    var rnum = [];
+    for (var i = 0; i < num; i++) {
+        rnum.push(Math.random());
+    }
+    return rnum;
+}
 
